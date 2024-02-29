8e17aeef76f87f449be7b60b9ed36594f8210f95
uttkarsh-8 <uttkarsh.srivastava12@gmail.com>
2024-02-29 23:49:52 +0530

    Initial commit

---
+++ src/test/java/com/blog_rest_api/springbootrestapi/SpringbootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog_rest_api.springbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
